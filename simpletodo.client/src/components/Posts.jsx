5e541092bc3b1a99a8d9dfb7cc04a9728583211a
IslamT1 <90341747+IslamT1@users.noreply.github.com>
2024-02-09 15:42:29 +0300

    fixed modal box

--- simpletodo.client/src/components/Posts.jsx
+++ simpletodo.client/src/components/Posts.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import ModalButton from "./ModalBtn";
+import React, { useEffect, useState } from "react";
 
 const URL = "api/Posts";
 const Posts = () => {
@@ -77,12 +76,40 @@ const Posts = () => {
   return (
     <div class="posts">
       <div class="child">
-        <p>Posts creation</p>
-        <div style={{ margin: "10px" }}>
-          <input id="header" type="text" />
-        </div>
+        <p
+          style={{
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#bbbbbb",
+            textAlign: "center",
+            margin: "20px 0",
+            textTransform: "uppercase",
+          }}
+        >
+          Posts creation
+        </p>
         <div>
-          <textarea id="text" />
+          <input
+            id="header"
+            type="text"
+            class="inputPostCreation"
+            placeholder="Enter title..."
+          />
+        </div>
+        <div
+          style={{
+            marginBottom: "10px",
+            border: "2px solid #cccccc40",
+            borderRadius: "5px",
+            padding: "10px",
+            boxSizing: "border-box",
+          }}
+        >
+          <textarea
+            id="text"
+            class="textareaPostCreation"
+            placeholder="Enter text..."
+          />
         </div>
         <button onClick={() => addPost()}>Add post</button>
       </div>
@@ -107,37 +134,93 @@ const PostItem = ({ post, deleteAction, updateAction }) => {
     <div class="postitems">
       <h2>{post.header}</h2>
       <p>{post.text}</p>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <button
           style={{ marginRight: "5px" }}
           onClick={() => deleteAction(post.id)}
         >
           Delete
         </button>
-        <ModalButton
-          btnName={"Update"}
-          title={"Update post"}
-          modalContent={
+        <BasicModal>
+          <div>
             <div>
-              <div style={{ margin: "10px" }}>
-                <input
-                  id="header"
-                  type="text"
-                  defaultValue={post.header}
-                  onChange={(e) => (post.header = e.target.value)}
-                />
-              </div>
-              <div>
-                <textarea
-                  id="text"
-                  defaultValue={post.text}
-                  onChange={(e) => (post.text = e.target.value)}
-                />
-              </div>
-              <button onClick={() => updateAction(post)}>Update post</button>
+              <input
+                id="header"
+                class="inputPostCreation"
+                type="text"
+                defaultValue={post.header}
+                onChange={(e) => (post.header = e.target.value)}
+              />
             </div>
-          }
-        />
+            <div style={{ marginTop: "6px" }}>
+              <textarea
+                id="text"
+                class="textareaPostCreation"
+                defaultValue={post.text}
+                onChange={(e) => (post.text = e.target.value)}
+                style={{ marginBottom: "10px", resize: "none" }}
+              />
+            </div>
+            <button type="button" onClick={() => updateAction(post)}>
+              Update post
+            </button>
+          </div>
+        </BasicModal>
+      </div>
+    </div>
+  );
+};
+
+function BasicModal({ children }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        Update
+      </button>
+      <Modal isOpen={open} onClose={handleClose}>
+        <>{children}</>
+      </Modal>
+    </div>
+  );
+}
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      // onClick={onClose}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        background: "rgba(0, 0, 0, 0.5)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          background: "black",
+          margin: "auto",
+          padding: "15px",
+          border: "2px solid #333333",
+          borderRadius: "8px",
+          boxShadow: "2px solid black",
+          transform: "translateX(-75%)",
+        }}
+      >
+        {children}
+        <button style={{ marginTop: "5px" }} type="button" onClick={onClose}>
+          Close
+        </button>
       </div>
     </div>
   );
